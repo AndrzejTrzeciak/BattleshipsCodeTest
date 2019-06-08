496a0e6694cb73dfe82eb9b874c9a33cc8405070
Andrzej Trzeciak <atrzecia@gmail.com>
2019-06-09 01:27:30 +0200

    definicja modelu

---
+++ AppCore/Model/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Model
+{
+    public class Cell
+    {
+        public bool IsCloaked { get; set; }
+        public bool IsHit { get; set; }
+        public bool IsOccupied { get; set; }
+        public Coordinates Coordinates { get; set; }
+    }
+}
---
+++ AppCore/Model/Coordinates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Model
+{
+
+    public class Coordinates : IEquatable<Coordinates>
+    {
+        public short X;
+        public short Y;
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Coordinates);
+        }
+
+        public bool Equals(Coordinates other)
+        {
+            return other != null &&
+                   X == other.X &&
+                   Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+    }
+}
